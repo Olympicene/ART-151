565ac20e33395abea089aea3e382dacc6f691675
Kevin <kevinjcord@gmail.com>
2024-02-21 10:26:19 -0600

    got somewhere

--- Project_2/sketch.js
+++ Project_2/sketch.js
@@ -1,7 +1,37 @@
-function setup() {
-  createCanvas(400, 400);
+let song;
+let isLoaded = false;
+
+function preload() {
+  song = loadSound('tekka.mp3')
 }
 
+function setup() {
+  let cnv = createCanvas(windowWidth, windowHeight);
+  cnv.mouseClicked(togglePlay);
+  frameRate(60);
+  amplitude = new p5.Amplitude(0.5);
+  amplitude.setInput(song);
+
+}
+
+
 function draw() {
-  background(220);
+  background(255, 255, 255, 100)
+  // fill(255, 255, 255);
+  strokeWeight(10)
+
+  let level = amplitude.getLevel();
+  let size = map(level, 0, 0.3, 100, 300)
+
+  ellipse(height / 2, width / 2, size);
+  text(size, width / 4, height / 4,)
+}
+
+
+function togglePlay() {
+  if (song.isPlaying()) {
+    song.pause();
+  } else {
+    song.play();
+  }
 }
